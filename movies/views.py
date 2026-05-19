5eadd78f68783a38d2abcce20db52577336af37e
Sebastian Tenorio <joantenorio89@gmail.edu.ec>
2026-05-19 09:55:20 -0500

    Examen finalizado

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
 
-# Create your views here.
+def index(request):
+    movies = Movie.objects.all()
+    return render(request, 'index.html', {'movies': movies})
+
+def detalle(request, movie_id):
+    movie = get_object_or_404(Movie, id=movie_id)
+    return render(request, 'detalle.html', {'movie': movie})
